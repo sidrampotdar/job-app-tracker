d661c2b0da0060ddbb608cc400e1a0bd06d5e310
Sidramappa Potdar <126233261+sidrampotdar@users.noreply.github.com>
2026-04-13 23:14:17 +0530

    Sign In added

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -7,11 +7,41 @@ import {
   CardFooter,
   CardContent,
 } from "@/components/ui/card";
+import { useRouter } from "next/navigation";
+
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { signIn } from "@/lib/auth/auth-client";
+
 import Link from "next/link";
 const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault(); // Prevent the default form submission behavior - refreshing the page
+    setError("");
+    setLoading(true);
+    try {
+      const result = await signIn.email({ email, password });
+
+      if (result.error) {
+        setError(result.error.message || "An error occurred during sign in.");
+      } else {
+        router.push("/dashboard");
+      }
+    } catch (error) {
+      setError("Failed to Sign in.");
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
     <div className="flex min-h-[calc(100vh-4rem)] items-center justify-center bg-white p-4">
       <Card className="w-full max-w-md border-gray-200 shadow-lg">
@@ -23,9 +53,14 @@ const SignIn = () => {
             Enter your credentials to access your account
           </CardDescription>
         </CardHeader>
-        <form className="space-y-4">
+        <form onSubmit={handleSubmit} className="space-y-4">
           <CardContent className="space-y-4">
-            <div className="rounded-md bg-destructive/15 p-3 text-sm text-destructive"></div>
+            {error && (
+              <div className="rounded-md bg-destructive/15 p-3 text-sm text-destructive">
+                {" "}
+                {error}
+              </div>
+            )}
 
             <div className="space-y-2">
               <Label htmlFor="email" className="text-gray-700">
@@ -35,6 +70,8 @@ const SignIn = () => {
                 id="email"
                 type="email"
                 placeholder="you@example.com"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 required
                 className="border-gray-300 focus:border-primary focus:ring-primary"
               />
@@ -47,6 +84,8 @@ const SignIn = () => {
                 id="password"
                 type="password"
                 required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 minLength={8}
                 className="border-gray-300 focus:border-primary focus:ring-primary"
               />
@@ -55,9 +94,10 @@ const SignIn = () => {
           <CardFooter className="flex flex-col space-y-4">
             <Button
               type="submit"
+              disabled={loading}
               className="w-full bg-primary hover:bg-primary/90"
             >
-              Sign In
+              {loading ? "Signing in..." : "Sign In"}
             </Button>
             <p className="text-center text-sm text-gray-600">
               Already have an account?{" "}
